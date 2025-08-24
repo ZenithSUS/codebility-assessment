52612ebbff9215020baf8224c579a19cacd5f64b
ZenithSUS <jeranmerino147@gmail.com>
2025-08-24 15:12:02 +0800

    feat: add helmet and xss middleware for enhanced security

--- backend/app.ts
+++ backend/app.ts
@@ -1,15 +1,19 @@
 import express, { Application } from "express";
 import { logger } from "./middleware/logger.js";
-import cors from "cors";
-import todoRouter from "./routes/todo-routes.js";
 import { notFound } from "./middleware/not-found.js";
+import { xss } from "express-xss-sanitizer";
+import cors from "cors";
+import helmet from "helmet";
+import todoRouter from "./routes/todo-routes.js";
 
 // Express Config
 const app: Application = express();
 
 // Middlewares
 app.use(logger);
+app.use(helmet());
 app.use(cors());
+app.use(xss());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
